1954334be23f22d04ea0f91fb58a0128473019e7
jonathanstyo <100742241+jonathanstyo@users.noreply.github.com>
2024-05-13 09:57:31 +0700

    landing page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Selamat Datang di Papua</h1>
+        <p>Temukan keindahan dan kekayaan budaya Papua di sini!</p>
+        <div className="Button-container">
+          <a href="#about" className="Button">Tentang Kami</a>
+          <a href="#objek-wisata" className="Button">Objek Wisata</a>
+          <a href="#makanan-khas" className="Button">Makanan Khas</a>
+          <a href="#adat-budaya" className="Button">Adat Budaya</a>
+          <a href="#lagu-daerah" className="Button">Lagu Daerah</a>
+          <a href="#sejarah" className="Button">Sejarah</a>
+        </div>
       </header>
     </div>
   );
